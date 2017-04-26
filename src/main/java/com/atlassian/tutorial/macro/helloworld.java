8b5111257e50949dd37b5cc5368521b2cab55aa9
Serhii Zinchuk <szi@ciklum.com>
2017-04-26 17:17:12 +0300

    Real table generation added

--- src/main/java/com/atlassian/tutorial/macro/helloworld.java
+++ src/main/java/com/atlassian/tutorial/macro/helloworld.java
@@ -36,11 +36,13 @@ public class helloworld implements Macro {
         pageBuilderService.assembler().resources().requireWebResource("com.atlassian.tutorial.myConfluenceMacro:myConfluenceMacro-resources");
         //return generateSimpleHtmlTable();
 
-      //  return generateTableWithDivs();
+        //  return generateTableWithDivs();
 
-      //  return generateTableWithDivsFull();
+        //  return generateTableWithDivsFull();
 
-        return checkStructure();
+        // return checkStructure();
+
+        return generateProgramReport();
     }
 
     public BodyType getBodyType() {
@@ -360,8 +362,8 @@ public class helloworld implements Macro {
                             "  </div>");
 
                     Page programPage = titleToProgram.get(programTitleIterator);
-                   // List<String> projectTitlesForProgram = parser.findProjectsForProgram(programPage.getBodyContents().get(0).getContent().getBodyAsString());
-                  //  System.out.println("Project titles for program: " + projectTitlesForProgram);
+                    // List<String> projectTitlesForProgram = parser.findProjectsForProgram(programPage.getBodyContents().get(0).getContent().getBodyAsString());
+                    //  System.out.println("Project titles for program: " + projectTitlesForProgram);
                     List<Project> projects = parser.parseDataForProgram(programPage.getBodyContents().get(0).getContent().getBodyAsString());
                     //Draw table for only existing projects for Program
                     if (!projects.isEmpty()) {
@@ -373,7 +375,7 @@ public class helloworld implements Macro {
                                     "    <div class=\"macro-column objectives\">+</div>\n" +
                                     "    <div class=\"macro-column outcome\">+</div>\n" +
                                     "    <div class=\"macro-column outcome\">" + projectTitleForProgramIterator.getOwner() + "</div>\n" +
-                                    "    <div class=\"macro-column deadline\">" + projectTitleForProgramIterator.getDeadline() +"</div>\n" +
+                                    "    <div class=\"macro-column deadline\">" + projectTitleForProgramIterator.getDeadline() + "</div>\n" +
                                     "    <div class=\"macro-column missed\">24.03.2017</div>\n" +
                                     "    <div class=\"macro-column actions\">Do something!</div>\n" +
                                     "  </div>");
@@ -395,7 +397,7 @@ public class helloworld implements Macro {
         return builder.toString();
     }
 
-    public String checkStructure(){
+    public String checkStructure() {
         PageManager pageManager = (PageManager) ContainerManager.getComponent("pageManager");
 
         Space TEST_SPACE = spaceManager.getSpace(SPACE_KEY);
@@ -428,7 +430,7 @@ public class helloworld implements Macro {
             titleToProgram.put(programPage.getTitle(), programPage);
         }
 
-    
+
 //        Page goalPage = goalPages.get(0);
 //
 //        String content = goalPage.getBodyContents().get(0).getContent().getBodyAsString();
@@ -447,5 +449,183 @@ public class helloworld implements Macro {
 
         ExtendedProject projectObject = parser.parseDataForExtendedProject(content);
         return projectObject.toString();
+    }
+
+    public String generateProgramReport() {
+        PageManager pageManager = (PageManager) ContainerManager.getComponent("pageManager");
+
+        Space TEST_SPACE = spaceManager.getSpace(SPACE_KEY);
+
+        List<Page> PagesInTestSpace = pageManager.getPages(TEST_SPACE, false);
+
+        Label goal = new Label("goal");
+        Label program = new Label("program");
+        Label project = new Label("project");
+
+        List<Page> goalPages = new ArrayList<Page>();
+        List<Page> programPages = new ArrayList<Page>();
+        List<Page> projectPages = new ArrayList<Page>();
+
+        for (Page page : PagesInTestSpace) {
+
+            if (page.getLabels().contains(goal)) {
+                goalPages.add(page);
+            }
+            if (page.getLabels().contains(program)) {
+                programPages.add(page);
+            }
+            if (page.getLabels().contains(project)) {
+                projectPages.add(page);
+            }
+
+        }
+
+        Map<String, Page> titleToProjectPages = new LinkedHashMap<String, Page>();
+        Map<String, Page> titleToProgramPages = new LinkedHashMap<String, Page>();
+        Map<String, Page> titleToGoalPages = new LinkedHashMap<String, Page>();
+
+        Map<String, ExtendedProject> titleToProjectObject = new LinkedHashMap<String, ExtendedProject>();
+        Map<String, Program> titleToProgramObject = new LinkedHashMap<String, Program>();
+        Map<String, Goal> titleToGoalObject = new LinkedHashMap<String, Goal>();
+
+        List<ExtendedProject> projectObjects = new ArrayList<ExtendedProject>();
+        List<Goal> goalObjects = new ArrayList<Goal>();
+        List<Program> programObjects = new ArrayList<Program>();
+
+        for (Page page : goalPages) {
+            titleToGoalPages.put(page.getTitle(), page);
+            String content = page.getBodyContents().get(0).getContent().getBodyAsString();
+            Goal parsedGoal = parser.parseDataForGoal(content);
+            goalObjects.add(parsedGoal);
+            titleToGoalObject.put(page.getTitle(), parsedGoal);
+
+        }
+        for (Page page : programPages) {
+            titleToProgramPages.put(page.getTitle(), page);
+            String content = page.getBodyContents().get(0).getContent().getBodyAsString();
+            Program parsedProgram = parser.parseDataForExtendedProgram(content);
+            programObjects.add(parsedProgram);
+            titleToProgramObject.put(page.getTitle(), parsedProgram);
+        }
+        for (Page page : projectPages) {
+            titleToProjectPages.put(page.getTitle(), page);
+            String content = page.getBodyContents().get(0).getContent().getBodyAsString();
+            ExtendedProject parsedProject = parser.parseDataForExtendedProject(content);
+            projectObjects.add(parsedProject);
+            titleToProjectObject.put(page.getTitle(), parsedProject);
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("<div class=\"container-fluid\" style=\"margin-top: 10px\">\n" +
+                "  <div class=\"macro-table\">");
+        builder.append("<div class=\"macro-table-row header\">\n" +
+                "    <div class=\"macro-column goals\">Goals</div>\n" +
+                "    <div class=\"macro-column programs\">Programs</div>\n" +
+                "    <div class=\"macro-column projects\">Projects</div>\n" +
+                "    <div class=\"macro-column objectives\">Objectives</div>\n" +
+                "    <div class=\"macro-column outcome\">Outcome</div>\n" +
+                "    <div class=\"macro-column outcome\">Owner</div>\n" +
+                "    <div class=\"macro-column deadline\">Deadline</div>\n" +
+                "    <div class=\"macro-column missed\">Missed deadlines</div>\n" +
+                "    <div class=\"macro-column actions\">Dependency and Actions</div>\n" +
+                "  </div>");
+
+        for (Goal goalIterator : goalObjects) {
+            builder.append("<div class=\"macro-table-row\">\n" +
+                    "    <div class=\"macro-column goals\"> " + goalIterator.getTitle() + " </div>\n" +
+                    "    <div class=\"macro-column programs\"></div>\n" +
+                    "    <div class=\"macro-column projects\"></div>\n" +
+                    "    <div class=\"macro-column objectives\"></div>\n" +
+                    "    <div class=\"macro-column outcome\"></div>\n" +
+                    "    <div class=\"macro-column outcome\"></div>\n" +
+                    "    <div class=\"macro-column deadline\"></div>\n" +
+                    "    <div class=\"macro-column missed\"></div>\n" +
+                    "    <div class=\"macro-column actions\"></div>\n" +
+                    "  </div>");
+
+            List<Program> programsForGoal = findProgramsForGoal(programObjects, goalIterator.getTitle());
+            //Draw table for only existing programs for goal
+            if (!programsForGoal.isEmpty()) {
+                for (Program programIterator : programsForGoal) {
+
+                    builder.append("<div class=\"macro-table-row\">\n" +
+                            "    <div class=\"macro-column goals\"></div>\n" +
+                            "    <div class=\"macro-column programs\"> " + programIterator.getTitle() + "</div>\n" +
+                            "    <div class=\"macro-column projects\"></div>\n" +
+                            "    <div class=\"macro-column objectives\">" + programIterator.getObjectives() +"</div>\n" +
+                            "    <div class=\"macro-column outcome\">" +  programIterator.getOutcome()+"</div>\n" +
+                            "    <div class=\"macro-column outcome\">"+  programIterator.getOwner()+"</div>\n" +
+                            "    <div class=\"macro-column deadline\">"+  programIterator.getDeadline()+"</div>\n" +
+                            "    <div class=\"macro-column missed\">"+  programIterator.getMissedDeadline()+"</div>\n" +
+                            "    <div class=\"macro-column actions\">" +  programIterator.getDependencyAndAction()+"</div>\n" +
+                            "  </div>");
+
+//                    Page programPage = titleToProgram.get(programIterator);
+//                    // List<String> projectTitlesForProgram = parser.findProjectsForProgram(programPage.getBodyContents().get(0).getContent().getBodyAsString());
+//                    //  System.out.println("Project titles for program: " + projectTitlesForProgram);
+//                    List<Project> projects = parser.parseDataForProgram(programPage.getBodyContents().get(0).getContent().getBodyAsString());
+//                    //Draw table for only existing projects for Program
+                    List<ExtendedProject> projectsForProgram = findProjectsForProgram(projectObjects, programIterator.getTitle());
+                    if (!projectsForProgram.isEmpty()) {
+                        for (ExtendedProject projectForProgramIterator : projectsForProgram) {
+                            builder.append(" <div class=\"macro-table-row\">\n" +
+                                    "    <div class=\"macro-column goals\"></div>\n" +
+                                    "    <div class=\"macro-column programs\"></div>\n" +
+                                    "    <div class=\"macro-column projects\">" + projectForProgramIterator.getTitle() + "</div>\n" +
+                                    "    <div class=\"macro-column objectives\">"+ projectForProgramIterator.getObjectives()+ "</div>\n" +
+                                    "    <div class=\"macro-column outcome\">" + projectForProgramIterator.getOutcome()+"</div>\n" +
+                                    "    <div class=\"macro-column outcome\">" + projectForProgramIterator.getOwner() + "</div>\n" +
+                                    "    <div class=\"macro-column deadline\">" + projectForProgramIterator.getDeadline() + "</div>\n" +
+                                    "    <div class=\"macro-column missed\">"+ projectForProgramIterator.getMissedDeadline()+ "</div>\n" +
+                                    "    <div class=\"macro-column actions\">" + projectForProgramIterator.getDependencyAndAction() + "</div>\n" +
+                                    "  </div>");
+                        }
+                    } else {
+//                        builder.append("<tr>");
+//                        builder.append("<td>" + goalIterator.getTitle() + "</td>");
+//                        builder.append("<td>" + programTitleIterator + "</td>");
+//                        builder.append("<td>" + "</td>");
+//                        builder.append("</tr>");
+                    }
+                }
+            }
+        }
+
+        builder.append("</div>\n" +
+                "</div>");
+
+        return builder.toString();
+
+
+//        Page projectPage = projectPages.get(0);
+//
+//        String content = projectPage.getBodyContents().get(0).getContent().getBodyAsString();
+//
+//        ExtendedProject projectObject = parser.parseDataForExtendedProject(content);
+//        return projectObject.toString();
+    }
+
+    private List<Program> findProgramsForGoal(List<Program> programObjects, String goalLink) {
+        List<Program> result = new ArrayList<Program>();
+        for (Program program : programObjects) {
+            System.out.println("program.getLinkToGoal():" + program.getLinkToGoal());
+            System.out.println("goalLink:" + goalLink);
+            if (program.getLinkToGoal().equals(goalLink)) {
+                result.add(program);
+            }
+        }
+        return result;
+    }
+
+    private List<ExtendedProject> findProjectsForProgram(List<ExtendedProject> projectObjects, String programLink) {
+        List<ExtendedProject> result = new ArrayList<ExtendedProject>();
+        for (ExtendedProject project : projectObjects) {
+
+            if (project.getLinkToProgram().equals(programLink)) {
+                result.add(project);
+            }
+        }
+        return result;
     }
 }
